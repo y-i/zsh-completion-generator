06f53c049ffb3ce9b6f9f82b7a36c9155680c5ca
y-i <y-i@users.noreply.github.com>
2017-12-15 15:09:40 +0900

    (ruby) 複数階層ver.

--- zsh-completion.rb
+++ zsh-completion.rb
@@ -1,6 +1,38 @@
 #!/usr/bin/env ruby
 require 'yaml'
 
+def createCompilcatinWithDepth(cmd, names, num)
+    subcmds = []
+    cmd['subcmds'].each{ |subcmd|
+        subcmds.push("#{subcmd['command']}:#{subcmd['description']}")
+    }
+    funcname = names.join('__')
+    puts "_#{funcname}(){"
+
+    print "\tsubcmds=("
+    print subcmds.join(' ')
+    print ")\n"
+
+    print "\t_arguments\\\n"
+    print "\t\t'#{num}:first arg:{_describe commands subcmds}' \\\n"
+    print "\t\t'*: :->rests'\n"
+
+    print "\tcase $state in\n"
+    print "\t\trests)\n"
+    print "\t\t\t_#{funcname}__${line[#{num}]}\n"
+    print "\t\t;;\n"
+    print "\tesac\n"
+
+    puts "}"
+    puts ''
+    cmd['subcmds'].each{ |subcmd|
+        if subcmd.key?('subcmds')
+            names.push(subcmd['command'])
+            createCompilcatinWithDepth(subcmd, names, num+1)
+        end
+    }
+end
+
 yaml = YAML.load_file("test.yml")
 
 yaml.each{ |cmd|
@@ -16,7 +48,23 @@ yaml.each{ |cmd|
     print ")\n"
 
     print "\t_arguments\\\n"
-    print "\t\t'1:first arg:{_describe commands subcmds}'\n"
+    print "\t\t'1:first arg:{_describe commands subcmds}' \\\n"
+    print "\t\t'*: :->rests'\n"
+
+    print "\tcase $state in\n"
+    print "\t\trests)\n"
+    print "\t\t\t_#{cmd['name']}__${line[1]}\n"
+    print "\t\t;;\n"
+    print "\tesac\n"
 
     puts "}"
+    puts ''
+    cmd['subcmds'].each{ |subcmd|
+        if subcmd.key?('subcmds')
+            names = [cmd['name']]
+            names.push(subcmd['command'])
+            createCompilcatinWithDepth(subcmd, names, 2)
+        end
+    }
 }
+
